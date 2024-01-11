b18b9391fa7cc9786c98793b2ea667f55c75076a
urjimyu <aliyah_7@naver.com>
2024-01-11 11:41:41 +0000

    docs: 리드미 작성 (#24)

--- src/recoil/atoms.ts
+++ src/recoil/atoms.ts
@@ -1,8 +1,16 @@
-//이런 식으로 작성
+import { atom } from 'recoil';
 
-//import { atom } from "recoil";
+export const memberIdState = atom({
+  key: 'memberIdState',
+  default: 0,
+});
 
-// export const SweetState = atom({
-//   key: "sweet",
-//   default: {},
-// });
+export const nickNameState = atom({
+  key: 'nickNameState',
+  default: '',
+});
+
+export const profileImageState = atom({
+  key: 'profileImageState',
+  default: '',
+});
--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -1,7 +1,13 @@
 import { createBrowserRouter } from 'react-router-dom';
-import Main from '../pages/Main';
 import Mypage from '../pages/Mypage';
 import Layout from '../layouts/Layout';
+import GiftHomeDetailFriends from '../pages/GiftHomeDetail/GiftHomeDetailFriends';
+import GiftHomeDetail2030 from '../pages/GiftHomeDetail/GiftHomeDetail2030';
+import OnBoardingPage from '../pages/OnBoardingPage';
+import TournamentOngoing from '../pages/TournamentOngoing';
+import TournamentPage from '../pages/TournamentPage';
+import Start from '../pages/Start/Start';
+import Login from '../components/Login';
 
 const router = createBrowserRouter([
   {
@@ -10,11 +16,37 @@ const router = createBrowserRouter([
     children: [
       {
         path: '/',
-        element: <Main />,
+        element: <Start />,
+      },
+      {
+        path: '/api/oauth/kakao/login',
+        element: <Login />,
       },
       {
         path: '/mypage',
         element: <Mypage />,
+      },
+      {
+        // 임의로 세부페이지 추가
+        path: '/giftdetailfriends',
+        element: <GiftHomeDetailFriends />,
+      },
+      {
+        // 임의로 세부페이지 추가
+        path: '/giftdetail2030',
+        element: <GiftHomeDetail2030 />,
+      },
+      {
+        path: '/onboarding',
+        element: <OnBoardingPage />,
+      },
+      {
+        path: '/tournamentPro',
+        element: <TournamentOngoing />,
+      },
+      {
+        path: '/tournament',
+        element: <TournamentPage />,
       },
     ],
   },
